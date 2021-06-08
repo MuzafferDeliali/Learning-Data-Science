74c4ff2b4f45abd917b3e25a1db1a78f612eb7ca
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-06-08 16:12:17 +0300

    Update dataframe variable statistics and how we should read them.r
    Added class conversation and  removed unwanted datas from data frame

--- R/dataframe variable statistics and how we should read them.r
+++ R/dataframe variable statistics and how we should read them.r
@@ -18,3 +18,22 @@ var(singapore$price, na.rm = T) #the square of the standard deviation is equal t
 table(singapore$room_type) #categorical representation of room_type data with variable name and the frequency
 
 quantile(singapore$price, na.rm = T) #There is a big gap between %75 and %100 that means %75 of data values are between 0 and 199 only %25 is bigger than 199(i should check 199 and 200 are included or not) 
+
+
+#for better View we should remove them from data frame but we can't remove them rightnow
+#firstly we should do class conversation and then we will remove them from data frame
+
+class(singapore$room_type)
+singapore$room_type <- as.character(singapore$room_type)
+table(singapore$room_type)
+inds <- which( singapore$room_type == '' | singapore$room_type == '110' | 
+                 singapore$room_type == '1200' | singapore$room_type == '294' | 
+                 singapore$room_type == '31' | singapore$room_type == '40' | 
+                 singapore$room_type == '50' | singapore$room_type == '60' | 
+                 singapore$room_type == '74' | singapore$room_type == '75' | 
+                 singapore$room_type == '750' | singapore$room_type == '299' )
+
+singapore <- singapore[-inds , ]
+
+
+table(singapore$room_type)
