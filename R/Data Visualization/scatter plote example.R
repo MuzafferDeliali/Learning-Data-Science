58b6199e56b40e1aef4f018c9af6cf63e573d120
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-06-24 14:29:18 +0300

    Create scatter plote example.R

---
+++ R/Data Visualization/scatter plote example.R
@@ -0,0 +1,27 @@
+
+colors <- c("blue" , "red" , "green" , "black" , "purple" , "cornsilk" , "pink" , "yellow" , "cyan" , "brown")
+
+par(mar = c(5,5,5,5) , xpd = TRUE)
+plot(x = chess$Standard_Rating , y = chess$Rapid_rating ,
+     bty = "L" ,
+     xlab = "Standard Rating" ,
+     ylab = "Rabid Rating" ,
+     cex = 0.3 ,
+     pch = 19 ,
+     col = colors[as.factor(chess$Title)]
+     )
+
+legend(x = "bottomleft" , legend = levels(as.factor(chess$Title)) ,
+       title = "Titles" ,
+       pch = 19 ,
+       col = colors ,
+       cex = 0.7 ,
+       inset = c(-0.23 , -0.3) 
+       )
+
+par( xpd = FALSE) 
+abline( lm(chess$Standard_Rating ~ chess$Rapid_rating))
+
+unique(chess$Title)
+colors
+
