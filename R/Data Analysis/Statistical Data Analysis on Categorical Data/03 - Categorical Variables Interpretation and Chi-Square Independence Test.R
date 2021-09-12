e4fcd5d1faa89c74ee195ca072759d178fa40a9f
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-09-12 13:36:24 +0300

    Update 03 - Categorical Variables Interpretation and Chi-Square Independence Test.R

--- R/Data Analysis/Statistical Data Analysis on Categorical Data/03 - Categorical Variables Interpretation and Chi-Square Independence Test.R
+++ R/Data Analysis/Statistical Data Analysis on Categorical Data/03 - Categorical Variables Interpretation and Chi-Square Independence Test.R
@@ -37,5 +37,35 @@ library(tidyverse)
 df2 <- df1%>% filter (x %in% c('Katýlýyorum' , 'Katýlmýyorum') & 
                       y %in% c('Katýlýyorum' , 'Katýlmýyorum'))
                       
-
 df1$x %in% c('Katýlýyorum' , 'Katýlmýyorum')
+head(df2)
+table(df2)
+
+fisher.test(table(df2))
+# p-value is greater than  0.05 so odd ratio is equal to 1
+# If it is greater than 1, it is positively similar. If it's less it negative similarity
+
+# An odds ratio of 1.5 means the odds of the outcome in group A happening are 
+# one and a half times the odds of the outcome happening in group B
+
+# How to calculate odds ration on paper:
+# Odd ratio = (a/c) / (b/d)  
+# in our case it's  (9/3) / (7/1) = 3/7 
+
+# How to calculate reporting odds ration(ROR) on paper:
+# ROR = (a/b) / (c/d)
+
+fisher.test(table(df1))
+# H0: The two variables are independent of each other. No relationship.
+# Ha: The two variables are dependent of each other. Relationship exist. 
+
+# H0 is accepted.
+
+table(df2)
+# 2 x 2 odds ratio table
+fisher.test(table(df2))
+fisher.test(table(df2) , alternative = 'less')
+fisher.test(table(df2) , alternative = 'greater')
+
+# 5 x5 table
+fisher.test(table(df1))
