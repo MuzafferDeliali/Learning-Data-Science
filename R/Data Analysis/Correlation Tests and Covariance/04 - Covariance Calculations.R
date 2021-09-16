f382e6747292c80834a7c04993a951157562e3df
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-09-16 17:08:57 +0300

    Create 04 - Covariance Calculations.R

---
+++ R/Data Analysis/Correlation Tests and Covariance/04 - Covariance Calculations.R
@@ -0,0 +1,8 @@
+# Covariance calculations
+
+# Difference between correlation and covariance
+var(iris$Sepal.Length)
+var(iris$Sepal.Width)
+
+cov(iris$Sepal.Length , iris$Sepal.Width)
+cov(iris$Petal.Length , iris$Petal.Width)
