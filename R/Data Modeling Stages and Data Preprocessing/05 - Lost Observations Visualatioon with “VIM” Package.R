c1c54ce70147aae0ec088305f53447281110f3ff
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-10-24 21:59:58 +0300

    Create 05 - Lost Observations Visualatioon with “VIM” Package.R

---
+++ R/Data Modeling Stages and Data Preprocessing/05 - Lost Observations Visualatioon with “VIM” Package.R
@@ -0,0 +1,22 @@
+?VIM
+
+install.packages("VIM")
+library(VIM)
+
+names(df)
+
+new_df <- df[c("Acedamic.percentage.in.Operating.Systems" ,
+               "percentage.in.Algorithms" ,
+               "Percentage.in.Programming.Concepts" ,
+               "certifications" ,
+               "workshops")]
+
+View(new_df) 
+
+names(new_df) <- c("Operating" , "Algorithms" , "Concepts" , "Certification" , "Workshops")
+
+fig <- aggr(new_df , col = c("orange" , "red") , labels = names(new_df),
+            numbers = TRUE , sortVars = TRUE, cex.axis = 0.6 )
+
+
+marginplot( cbind(new_df$Operating , new_df$Algorithms) )
