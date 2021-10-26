72931efa3466b66a5acdd8c048826af521dd9b39
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-10-26 19:02:47 +0300

    Update 05 - Lost Observations Visualatioon with “VIM” Package.R

--- R/Data Modeling Stages and Data Preprocessing/05 - Lost Observations Visualatioon with “VIM” Package.R
+++ R/Data Modeling Stages and Data Preprocessing/05 - Lost Observations Visualatioon with “VIM” Package.R
@@ -2,6 +2,7 @@
 
 install.packages("VIM")
 library(VIM)
+library(ggplot2)
 
 names(df)
 
@@ -16,7 +17,8 @@ View(new_df)
 names(new_df) <- c("Operating" , "Algorithms" , "Concepts" , "Certification" , "Workshops")
 
 fig <- aggr(new_df , col = c("orange" , "red") , labels = names(new_df),
-            numbers = TRUE , sortVars = TRUE, cex.axis = 0.6 )
+            numbers = TRUE , sortVars = TRUE, cex.axis = 0.6 ,
+            ylab(c("Histogram of Missing Values" , "Pattern")))
 
 
 marginplot( cbind(new_df$Operating , new_df$Algorithms) )
