647230d7aee203a0a1426b027f9f878ef5866ccc
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2021-12-28 17:31:26 +0300

    Update 04 - Multiple Linear Regression.R

--- R/Machine Learning/04 - Multiple Linear Regression.R
+++ R/Machine Learning/04 - Multiple Linear Regression.R
@@ -6,8 +6,8 @@
 # Humidity Forecast Model 
 
 df_Albury <- weatherAUS[weatherAUS$Location == "Albury" ,]
-df_Albury <- df_Albury[c("Humidity9am" , "MinTemp" , "MaxTemp" ,
-                         "WindSpeed9am" , "Pressure9am" , "Temp9am")]
+df_Albury <- df_Albury[c("Humidity9am" , "MinTemp" , 
+                         "MaxTemp" , "WindSpeed9am" , "Pressure9am" , "Temp9am" , "Rainfall")]
 
 
 cor(df_Albury) # because of NA values it's all NA
@@ -28,6 +28,7 @@ md.pattern(df_Albury)
 #4 of them has missing value at humidty9am etc. etc.
 
 # Imputation of NA values with mice
+library(mice)
 ?mice
 
 imputed <- mice(df_Albury , m = 5)
@@ -35,3 +36,30 @@ imputed$imp
 
 df_Albury_imputed <- complete(imputed , 3) # we picked third imputation
 md.pattern(df_Albury_imputed) # Now we don't have any NA
+
+## Multiple Linear Regression 3 - Multiple Linear Regression Model ####
+
+set.seed(145)
+sampleIndex <- sample(1:nrow(df_Albury_imputed) , size = 0.8*nrow(df_Albury_imputed))
+
+trainSet <- df_Albury_imputed[sampleIndex , ]                      
+testSet <- df_Albury_imputed[-sampleIndex , ]                      
+
+View(trainSet)                      
+names(trainSet)
+model1 <- lm(Humidity9am ~ MinTemp + MaxTemp + Temp9am + 
+               Rainfall + WindSpeed9am + Pressure9am , data  = trainSet)
+
+# or instead typing all columns we can use "." 
+model1.a <- lm(Humidity9am ~ . , data  = trainSet)
+
+model1
+summary(model1)
+
+# seems like pressure doesn't effect , we can remove it
+
+model2 <- lm(Humidity9am ~ MinTemp + MaxTemp + Temp9am + 
+               Rainfall + WindSpeed9am , data  = trainSet)
+
+model2
+summary(model2)
