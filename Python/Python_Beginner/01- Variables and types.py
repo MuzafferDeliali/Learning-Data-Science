6ab9229a54083de018b6e6d9a12ce57bdb080919
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2022-01-24 20:28:50 +0300

    Create 01- Variables and types.py

---
+++ Python/Python_Beginner/01- Variables and types.py
@@ -0,0 +1,12 @@
+# Variables
+x = 5
+y = 10
+z  = (x * y)
+print(z)
+
+# Type function (class on R)
+a = type(z)
+print(a)
+
+#if we use it as z. spyder shows which functions  we can use with z variable
+z.bit_length()
