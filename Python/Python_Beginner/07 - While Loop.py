5fbb31ffad5f9846bdbe22cf1409d7f37704616f
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2022-02-02 21:43:42 +0300

    Create 07 - While Loop.py

---
+++ Python/Python_Beginner/07 - While Loop.py
@@ -0,0 +1,19 @@
+# While Loop
+
+x = 0
+while x <= 10:
+    print("the number is " , x)
+    x = x + 1
+
+# second way(tbh first way is much sense)
+x = 0
+while x <= 10:
+    print(f"the number is: {x} ")
+    x = x + 1
+    
+  
+MyList = [1,2,3,4,5,6,7,8,9,10]
+
+while 5 in MyList:
+    print(MyList)
+    MyList.pop()
