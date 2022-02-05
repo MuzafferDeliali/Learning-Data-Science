2d90c06f0873c03a3e5f7328bc23d95f7960dee5
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2022-02-05 19:57:02 +0300

    Update 10 - Calculator.py

--- Python/Python_Beginner/10 - Calculator.py
+++ Python/Python_Beginner/10 - Calculator.py
@@ -20,3 +20,27 @@ a = int(input("Type first int: "))
 b = int(input("Type second int "))
 islem = input("Pick what to do ")
 calculator(a,b,islem)
+
+# now let's do same thing with "while"
+def hesap(a,b,islemim):
+    if islemim == "+":
+        return (str(a)+" + "+str(b) + " = " +str(a+b))
+        
+    if islemim == "-":
+        return (str(a)+" - "+str(b) + " = " +str(a-b))
+        
+    if islemim == "*":
+        return (str(a)+" * "+str(b) + " = " +str(a*b))
+        result = a*b
+        print("Result is",result)
+        
+    if islemim == "/":
+        return (str(a)+" / "+str(b) + " = " +str(a/b))        
+    else:
+        print("Invalid Value")
+        
+while True:
+    a = int(input("Type first int: "))
+    b = int(input("Type second int "))
+    islemim = input("Pick what to do ")
+    print(hesap(a,b,islemim))
