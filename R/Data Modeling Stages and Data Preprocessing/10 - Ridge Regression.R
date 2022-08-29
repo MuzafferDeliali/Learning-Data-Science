9b591f354159ae343f327baf9fd3d807c74c59af
Muzaffer Deliali <deliali_muzaffer@hotmail.com>
2022-08-29 12:17:36 +0300

    Update 10 - Ridge Regression.R

--- R/Data Modeling Stages and Data Preprocessing/10 - Ridge Regression.R
+++ R/Data Modeling Stages and Data Preprocessing/10 - Ridge Regression.R
@@ -1,4 +1,4 @@
-## Data pre processing - Standardization
+## Data pre processing - Standardization####
 
 install.packages("glmnet", dependencies=TRUE)
 library(caret)
@@ -34,8 +34,9 @@ view(modelDataScaled)
 #   mutate(purose_cat = as.factor(purpose_cat)) %>%
 #   select(loan_amount , term , income_category , purpose_cat ,
 #          grade , interest_payments , loan_condition , annual_inc , emp_length_int)
-#
 
+
+install.packages(glmnet)
 ## One Hot Encoding Dummy Variable ####
 #we use dependand variable
 modelDataScaled1 <- model.matrix(loan_amount ~. , data = modelDataScaled)
@@ -44,7 +45,40 @@ head(modelDataScaled1)
 # dividing as test and train
 set.seed(145)
 sampleTrainIndex <- sample(1:nrow(modelDataScaled1) , size = 0.8*nrow(modelDataScaled1))
-trainSet <- modelDataScaled1[sampleTrainIndex,]
+trainSet <- modelDataScaled1[sampleTrainIndex]
 testSet <- modelDataScaled1[-sampleTrainIndex]
 
 # Modelling at Ridge Regression
+trainSet_x <- modelDataScaled1[sampleTrainIndex]
+testSet_x <- modelDataScaled1[-sampleTrainIndex]
+
+trainSet_y <- modelDataScaled$loan_amount[sampleTrainIndex]
+testSet_y <- modelDataScaled$loan_amount[-sampleTrainIndex]
+is.atomic(modelDataScaled$loan_amount)
+
+## Ridge Regression Model ####
+modelRidge1 <- glmnet(trainSet_x , trainSet_y , alpha = 0 , lambda = 0.05)
+summary(modelRidge1)
+
+?glmnet
+
+modelRidge1$beta
+modelRidge1$a0 #intercept value
+modelRidge1$lambda
+modelRidge1$dev.ratio # higher dev.ratio means better model
+
+## Cross validation lambda adjustments ####
+
+?cv.gmlnet
+
+lambdas = 10^seq(3 , -2 , by = -.01)
+lambdas
+
+modelRidgeCV<- cv.glmnet(trainSet_x , trainSet_y , alpha = 0 , lambda = lambdas , nfolds = 10)
+plot(modelRidgeCV)
+
+# Ideal lambda value
+modelRidgeCV$lambda.min
+
+modelRidgeCV
+ncol(trainSet_x)
